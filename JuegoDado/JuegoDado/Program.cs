1f9f8ee58302289deca1ef0b6412a3f78ffbdf0e
franklin_cuascota <franklin.cuascota@ister.edu.ec>
2026-02-24 23:55:38 -0500

    validacion entrada

--- JuegoDado/JuegoDado/Program.cs
+++ JuegoDado/JuegoDado/Program.cs
@@ -7,13 +7,22 @@ namespace JuegoDado
         static void Main(string[] args)
         {
             Console.WriteLine("=== JUEGO DE DADOS ===");
-            Console.WriteLine("Presiona ENTER para lanzar...");
-            Console.ReadKey();
+            bool valido = false;
+
+            while (!valido)
+            {
+                Console.WriteLine("Presiona ENTER para lanzar...");
+                var tecla = Console.ReadKey(true);
+
+                if (tecla.Key == ConsoleKey.Enter)
+                    valido = true;
+                else
+                    Console.WriteLine("Tecla inválida.");
+            }
 
             Random rnd = new Random();
             int resultado = rnd.Next(1, 7);
-
-            Console.WriteLine($"Resultado: {resultado}");
+            Console.WriteLine($"\nResultado: {resultado}");
         }
     }
 }
