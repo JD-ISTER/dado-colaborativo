fd18367d5934a4474be035eb7254ec65e1e49805
juandelpozo <juan.del@ister.edu.ec>
2026-02-24 20:47:54 -0800

    Primer commit jd

--- JuegoDado/JuegoDado/Program.cs
+++ JuegoDado/JuegoDado/Program.cs
@@ -4,7 +4,16 @@ namespace JuegoDado
 {
     class Program
     {
-      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== JUEGO DE DADOS ===");
+            Console.WriteLine("Presiona ENTER para lanzar...");
+            Console.ReadKey();
+
+            Random rnd = new Random();
+            int resultado = rnd.Next(1, 7);
+
+            Console.WriteLine($"Resultado: {resultado}");
         }
     }
 }
